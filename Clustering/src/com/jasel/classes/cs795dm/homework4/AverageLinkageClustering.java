17ef37629242cc11d362fcb3dce944f7723c09b5
Jasel <jasel@jasel.com>
2013-07-07 22:40:58 -0400

    Not complete yet.  Formulating to mimic functionality of SingleLinkageClustering - but need to figure out what to do after averaging distances of each...

--- Clustering/src/com/jasel/classes/cs795dm/homework4/AverageLinkageClustering.java
+++ Clustering/src/com/jasel/classes/cs795dm/homework4/AverageLinkageClustering.java
@@ -1,132 +1,82 @@
 package com.jasel.classes.cs795dm.homework4;
 
 import java.util.ArrayList;
-import java.util.zip.DataFormatException;
 
 
 public class AverageLinkageClustering {
-	private final static double HIGH_MIN = 100000000.0;
-
 	/**
 	 * @param args
-	 * @throws DataFormatException 
+	 * @throws InvalidEducationValueException 
 	 */
-	public static void main(String[] args) throws DataFormatException {
-		Instance minA = null;
-		Instance minB = null;
+	public static void main(String[] args) throws InvalidEducationValueException {
+		final double HIGH_MIN = 100000000.0;
+		final ClusterNumberFormat cnf = new ClusterNumberFormat();
+		
+		Cluster tempClusterI = null;
+		Cluster tempClusterJ = null;
+		Cluster minClusterA = null;
+		Cluster minClusterB = null;
+		Instance tempClusterIInstance = null;
+		Instance tempClusterJInstance = null;
+		Instance minInstanceA = null;
+		Instance minInstanceB = null;
 		
 		double dist = 0.0;
-		double min = HIGH_MIN;
+		double minDist = HIGH_MIN;
 		
-		ArrayList<Instance> instances = new ArrayList<Instance>();
-		ArrayList<Instance> cluster = new ArrayList<Instance>();
+		ArrayList<Cluster> clusters = new ArrayList<Cluster>();
 		
-		instances.add(new Instance("a", 0.1167, "BS", 0.1321, 0.0538, 0.0787));
-		instances.add(new Instance("b", 0.0833, "MS", 0.1132, 0.0000, 0.0562));
-		instances.add(new Instance("c", 0.1000, "BS", 0.0943, 0.0645, 0.1124));
-		instances.add(new Instance("d", 0.1833, "BS", 0.1321, 0.3326, 0.1685));
-		instances.add(new Instance("e", 0.1500, "MS", 0.1321, 0.1613, 0.1124));
-		instances.add(new Instance("f", 0.1000, "MS", 0.1509, 0.0215, 0.0787));
-		instances.add(new Instance("g", 0.1333, "BS", 0.0943, 0.2151, 0.2247));
-		instances.add(new Instance("h", 0.1333, "MS", 0.1509, 0.1613, 0.1685));
+		clusters.add(new Cluster("1", new Instance("a", 0.1167, "BS", 0.1321, 0.0538, 0.0787)));
+		clusters.add(new Cluster("2", new Instance("b", 0.0833, "MS", 0.1132, 0.0000, 0.0562)));
+		clusters.add(new Cluster("3", new Instance("c", 0.1000, "BS", 0.0943, 0.0645, 0.1124)));
+		clusters.add(new Cluster("4", new Instance("d", 0.1833, "BS", 0.1321, 0.3326, 0.1685)));
+		clusters.add(new Cluster("5", new Instance("e", 0.1500, "MS", 0.1321, 0.1613, 0.1124)));
+		clusters.add(new Cluster("6", new Instance("f", 0.1000, "MS", 0.1509, 0.0215, 0.0787)));
+		clusters.add(new Cluster("7", new Instance("g", 0.1333, "BS", 0.0943, 0.2151, 0.2247)));
+		clusters.add(new Cluster("8", new Instance("h", 0.1333, "MS", 0.1509, 0.1613, 0.1685)));
 		
-		for (int i = 0; i < instances.size(); i++) {
-			for (int j = i + 1; j < instances.size(); j++) {
-				dist = distance(instances.get(i), instances.get(j));
-				
-				if (dist < min) {
-					min = dist;
-					minA = instances.get(i);
-					minB = instances.get(j);
-				}
-				
-				System.out.print("DIST(" + instances.get(i).getName() + "," + instances.get(j).getName() + ") = ");
-				System.out.println(dist);
-			}
-		}
-			
-		System.out.println("Minimum is between " + minA.getName() + " and " + minB.getName() + " with value " + min);
-		System.out.println("Clustering these two instances.");
-		
-		// Add the two instances with the minimum distance to the cluster
-		cluster.add(minA);
-		cluster.add(minB);
-		
-		System.out.print("Instances in cluster: {");
-		for (int c = 0; c < cluster.size(); c++) {
-			System.out.print(cluster.get(c).getName());
-			
-			if (c < (cluster.size() - 1)) {
-				System.out.print(",");
-			}
-		}
-		
-		System.out.print("}\n");
-		
-		// Remove the two instances from the available (not-yet-clustered) instances
-		instances.remove(minA);
-		instances.remove(minB);
-		
-		System.out.print("Instances not yet clustered: {");
-		for (int i = 0; i < instances.size(); i++) {
-			System.out.print(instances.get(i).getName());
-			
-			if (i < (instances.size() - 1)) {
-				System.out.print(",");
-			}
-		}
-		
-		System.out.print("}\n");
-		
-		while (instances.size() > 0) {
-			min = HIGH_MIN;
-			
-			for (int c = 0; c < cluster.size(); c++) {
-				for (int i = 0; i < instances.size(); i++) {
-					dist = ClusterCalculation.distance(cluster.get(c), instances.get(i));
+		while (clusters.size() > 1) {
+			for (int i = 0; i < clusters.size(); i++) {
+				for (int j = i + 1; j < clusters.size(); j++) {
+					tempClusterI = clusters.get(i);
+					tempClusterJ = clusters.get(j);
 					
-					if (dist < min) {
-						min = dist;
-						minA = cluster.get(c);
-						minB = instances.get(i);
+					System.out.println("Cluster " + tempClusterI.getName() + " has Instance(s) " + tempClusterI.getInstancesNameSet());
+					System.out.println("Cluster " + tempClusterJ.getName() + " has Instance(s) " + tempClusterJ.getInstancesNameSet());
+					
+					for (int k = 0; k < tempClusterI.size(); k++) {
+						for (int l = 0; l < tempClusterJ.size(); l++) {
+							tempClusterIInstance = tempClusterI.get(k);
+							tempClusterJInstance = tempClusterJ.get(l);
+							
+							dist = ClusterCalculation.distance(tempClusterI.get(k), tempClusterJ.get(l));
+							
+							System.out.println("DIST(" + tempClusterIInstance.getName() + "," + tempClusterJ.getName() + ") = " +
+									cnf.format(dist));
+							
+							if (dist < minDist) {
+								minDist = dist;
+								minClusterA = tempClusterI;
+								minInstanceA = tempClusterIInstance;
+								minClusterB = tempClusterJ;
+								minInstanceB = tempClusterJInstance;
+							}
+						}
 					}
-					
-					System.out.print("DIST(" + cluster.get(c).getName() + "," + instances.get(i).getName() + ") = ");
-					System.out.println(dist);
 				}
 			}
-			
-			System.out.println("Minimum is between " + minA.getName() + " (already clustered) and " +
-					minB.getName() + " with value " + min);
-			System.out.println("Bringing " + minB.getName() + " into the cluster.");
-			
-			// Add to the cluster the closest instance (having the minimum distance to any instance within the cluster)
-			cluster.add(minB);
-			
-			System.out.print("Instances in cluster: {");
-			for (int c = 0; c < cluster.size(); c++) {
-				System.out.print(cluster.get(c).getName());
 				
-				if (c < (cluster.size() - 1)) {
-					System.out.print(",");
-				}
-			}
+			System.out.println("Minimum exists between Instance " + minInstanceA.getName() + " of Cluster " + minClusterA.getName() +
+					" and Instance " + minInstanceB.getName() + " of Cluster " + minClusterB.getName() +
+					" with a value of " + cnf.format(minDist));
+			System.out.println("Merging cluster " + minClusterB.getName() + " into cluster " + minClusterA.getName());
 			
-			System.out.print("}\n");
+			// minA and minB are the two clusters with the closest member Instance(s).
+			// Merge B into A and remove B from the list of Clusters.
+			minClusterA.merge(minClusterB);
+			clusters.remove(minClusterB);
 			
-			// Remove the two instances from the available (not-yet-clustered) instances
-			instances.remove(minB);
-			
-			System.out.print("Instances not yet clustered: {");
-			for (int i = 0; i < instances.size(); i++) {
-				System.out.print(instances.get(i).getName());
-				
-				if (i < (instances.size() - 1)) {
-					System.out.print(",");
-				}
-			}
-			
-			System.out.print("}\n");
+			System.out.println("Cluster " + minClusterA.getName() + " now contains instance(s) " + minClusterA.getInstancesNameSet());
 		}
 	}
 }
